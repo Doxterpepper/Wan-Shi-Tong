09e7cc88fc7155b84aa2d1861a009f7d4a3b69e5
doxterpepper <dockconnellyoneal@gmail.com>
2019-02-09 02:42:48 -0500

    got static files working, now for styling

--- fileserve.py
+++ fileserve.py
@@ -1,14 +1,24 @@
+""" Simple flask app for serving static files """
+
 import os
-from flask import Flask, render_template, request
+import re
+from flask import Flask, render_template, request, send_file
 import conf
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """ Empty path gives 404 so we need a separate endpoint for home """
     resources = conf.BaseConfig.FILES_BASE_PATH
     return render_template('index.html', files=os.listdir(resources), path='')
 
 @app.route('/<path:path>')
 def file_path(path):
+    """ Take an arbitrary path and return the files or file """
     resources = conf.BaseConfig.FILES_BASE_PATH
-    return render_template('index.html', files=os.listdir(resources+path), path=path)
+    print(path)
+    print(os.path.isdir(resources+path))
+    if os.path.isdir(resources+path):
+        return render_template('index.html', files=os.listdir(resources+path), path=path)
+
+    return send_file(resources+path)
--- wsgi_app.py
+++ wsgi_app.py
@@ -1,5 +1,5 @@
 import sys
 sys.path.append('/srv/http/')
 
-import app
-application = app.app
+import fileserve
+application = fileserve.app
