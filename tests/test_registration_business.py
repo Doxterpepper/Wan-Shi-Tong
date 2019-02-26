567c4d751d8dabcfd5de8ea0215d01970056a9b8
doxterpepper <dockconnellyoneal@gmail.com>
2019-02-26 00:12:05 -0500

    unit tests

---
+++ tests/test_registration_business.py
@@ -0,0 +1,68 @@
+""" Registration business tests """
+
+import datetime
+from unittest.mock import MagicMock
+import unittest
+from fileserve.business.registration_business import RegistrationBusiness
+from fileserve.conf import BaseConfig
+
+class TestRegistrationBusiness(unittest.TestCase):
+    """ Test cases for registration business """
+
+    def test_generate_code(self):
+        """ Test the generate_code method """
+        # Arrange
+        mock_data_handler = MagicMock()
+        user_business = RegistrationBusiness()
+        user_business.registration_datahandler = mock_data_handler
+
+        # Act
+        token = user_business.generate_code()
+
+        # Assert
+        mock_data_handler.save_code.assert_called_once()
+        self.assertTrue(len(token) > 0)
+
+    def test_check_registration_code(self):
+        """ Test the check_registration_code method """
+        # Arrange
+        BaseConfig.CODE_TIMEOUT = datetime.timedelta(hours=3)
+        delta = datetime.timedelta(hours=2)
+        time_stamp = datetime.datetime.utcnow() - delta
+
+        mock_data_handler = MagicMock()
+        mock_data_handler.retrieve_time_stamp.return_value = time_stamp
+        user_business = RegistrationBusiness()
+        user_business.registration_datahandler = mock_data_handler
+        valid = user_business.check_registration_code('')
+
+        self.assertTrue(valid)
+
+    def test_check_registration_code_fails(self):
+        """ Test negative test case for check_registration_code method """
+        BaseConfig.CODE_TIMEOUT = datetime.timedelta(hours=3)
+        delta = datetime.timedelta(hours=4)
+        time_stamp = datetime.datetime.utcnow() - delta
+
+        mock_data_handler = MagicMock()
+        mock_data_handler.retrieve_time_stamp.return_value = time_stamp
+        user_business = RegistrationBusiness()
+        user_business.registration_datahandler = mock_data_handler
+        valid = user_business.check_registration_code('')
+
+        self.assertFalse(valid)
+
+    def test_check_registration_code_failse_with_none(self):
+        """ Test negative test case when time_stamp is none """
+        BaseConfig.CODE_TIMEOUT = datetime.timedelta(hours=3)
+        time_stamp = None
+
+        mock_data_handler = MagicMock()
+        mock_data_handler.retrieve_time_stamp.return_value = time_stamp
+        user_business = RegistrationBusiness()
+        user_business.registration_datahandler = mock_data_handler
+        valid = user_business.check_registration_code('')
+
+        self.assertFalse(valid)
+
+
--- tests/test_user_business.py
+++ tests/test_user_business.py
@@ -11,25 +11,65 @@ class TestUserBusiness(unittest.TestCase):
 
     def test_validate(self):
         """ Test the validate method """
-        # Arrange
-        username = 'testusername'
-        password = 'password1!'
+        user_table = [
+            ('testusername', 'password1!', 'password1!', True),
+            ('testusername', 'password1!', 'password10', False),
+        ]
 
-        # Password retrieved will be a decoded password as a string
-        hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-        user = User(username, hashed_password)
+        for username, password, expectedpw, authenticated in user_table:
+            # Password retrieved will be a decoded password as a string
+            salt = bcrypt.gensalt()
+            hashed_password = bcrypt.hashpw(expectedpw.encode(), salt).decode()
+            user = User(username, hashed_password)
+
+            mock_data_handler = MagicMock()
+            mock_data_handler.get_user.return_value = user
+
+            user_business = business.user_business.UserBusiness()
+            user_business.user_datahandler = mock_data_handler
+
+            # Act
+            is_valid = user_business.validate(username, password)
+
+            # Assert
+            self.assertEqual(is_valid, authenticated)
+
+    def test_is_admin(self):
+        """ Test is_admin method """
+        admin_table = [
+            (1, True), # Is admin
+            (10, True), # Is admin
+            (0, False), # Not admin
+        ]
+
+        for user_level, admin in admin_table:
+            # Arrange
+            user = User('username', 'irrelavant password', user_level)
+            mock_data_handler = MagicMock()
+            mock_data_handler.get_user.return_value = user
+            user_business = business.user_business.UserBusiness()
+            user_business.user_datahandler = mock_data_handler
+
+            # Act
+            is_admin = user_business.is_admin('username')
+
+            # Assert
+            self.assertEqual(is_admin, admin)
+
+    def test_save(self):
+        """ Test the save method """
+        username = 'username'
+        password = 'password1!'
+        hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
         mock_data_handler = MagicMock()
-        mock_data_handler.get_user_password.return_value = user
-
+        correct_hash = None
         user_business = business.user_business.UserBusiness()
         user_business.user_datahandler = mock_data_handler
 
-        # Act
-        is_valid = user_business.validate(username, password)
-
-        # Assert
-        self.assertTrue(is_valid)
+        user_business.save('test_username', 'test_password')
+        mock_data_handler.save_user.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
+
