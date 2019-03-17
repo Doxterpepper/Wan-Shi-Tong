85ee1c232be815ac93df89f5073fc756b7c95727
doxterpepper <dockconnellyoneal@gmail.com>
2019-03-17 15:51:17 -0400

    Changing deploy flow

--- fileserve/database.py
+++ fileserve/database.py
@@ -1,9 +1,7 @@
 """ Database methods """
 
-import bcrypt
 import psycopg2
 from .conf import BaseConfig
-from .queries.user_queries import UserQueries
 
 class BaseDatabase:
     """ Database wrapper """
@@ -20,33 +18,3 @@ class BaseDatabase:
             )
 
         return self.connection
-
-def deploy():
-    """ Deploy the database tables """
-    db = BaseDatabase().get_connection()
-    with db.cursor() as cursor:
-        print('attempting create table users')
-        cursor.execute('''
-            DROP TABLE Users;
-            CREATE TABLE IF NOT EXISTS USERS 
-            (
-                Username      TEXT        PRIMARY KEY,
-                Password      TEXT,
-                UserLevel     NUMERIC     DEFAULT 0
-            );
-        ''')
-
-        default_username = 'dock'
-        default_password = bcrypt.hashpw('password1!'.encode(), bcrypt.gensalt())
-        cursor.execute(UserQueries.insert_user, [default_username, default_password.decode(), 1])
-
-        print('attempting to create registration code table')
-        cursor.execute('''
-            DROP TABLE RegistrationCodes;
-            CREATE TABLE IF NOT EXISTS RegistrationCodes
-            (
-                Code          TEXT,
-                TimeStamp     TEXT
-            );
-        ''')
-        db.commit()
